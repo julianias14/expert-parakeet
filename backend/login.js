3d1b23984b56777d1821e7067c847f887f94043c
ruzhue <ezhu669@gmail.com>
2026-04-18 21:13:09 -0400

    login fix fr?

--- backend/login.js
+++ backend/login.js
@@ -2,43 +2,52 @@ import { signup, login, observeAuth } from "./auth.js";
 
 // SIGN UP
 document.getElementById("signupBtn").onclick = async () => {
+
+  const suEmail = document.getElementById("suEmail");
+  const suPassword = document.getElementById("suPassword");
+  const suName = document.getElementById("suName");
+  const suAge = document.getElementById("suAge");
+
   const email = suEmail.value;
   const password = suPassword.value;
   const name = suName.value;
   const age = suAge.value;
 
   try {
-  await signup(email, password, name, age);
-  window.location.href = "home.html";
-} catch (err) {
-  if (err.code === "auth/email-already-in-use") {
-    showError("⚠️ Account already exists. Try logging in.");
-  } else if (err.code === "auth/weak-password") {
-    showError("⚠️ Password should be at least 6 characters.");
-  } else {
-    showError("⚠️ Something went wrong. Try again.");
+    await signup(email, password, name, age);
+    window.location.href = "home.html";
+  } catch (err) {
+    if (err.code === "auth/email-already-in-use") {
+      showError("⚠️ Account already exists. Try logging in.");
+    } else if (err.code === "auth/weak-password") {
+      showError("⚠️ Password should be at least 6 characters.");
+    } else {
+      showError("⚠️ Something went wrong. Try again.");
+    }
   }
-}
 };
 
 
 // LOGIN
 document.getElementById("loginBtn").onclick = async () => {
+  const liEmail = document.getElementById("liEmail");
+  const liPassword = document.getElementById("liPassword");
+  
   const email = liEmail.value;
   const password = liPassword.value;
 
   try {
-  await login(email, password);
-  window.location.href = "home.html";
-} catch (err) {
-  if (err.code === "auth/user-not-found") {
-    showError("⚠️ No account found.");
-  } else if (err.code === "auth/wrong-password") {
-    showError("⚠️ Incorrect password.");
-  } else {
-    showError("⚠️ Login failed.");
+    await login(email, password);
+    window.location.href = "home.html";
+  } catch (err) {
+    if (err.code === "auth/user-not-found") {
+      showError("⚠️ No account found.");
+    } else if (err.code === "auth/wrong-password") {
+      showError("⚠️ Incorrect password.");
+    } else {
+      showError("⚠️ Login failed.");
+    }
   }
-}
 };
 
 
